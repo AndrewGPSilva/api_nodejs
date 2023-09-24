ee0158e77f5845e0a1e29f82fbcb248aadd7a2fc
Andrew Gomes <andrewgomes1328@gmail.com>
2023-09-23 22:05:57 -0300

    api simples finalizada

--- src/app.js
+++ src/app.js
@@ -1,49 +1,24 @@
 import express from "express";
 import conectaNaDatabase from "./config/dbConnect.js";
-import livro from "./models/Livros.js";
+import routes from "./routes/index.js";
 
 const conexao = await conectaNaDatabase();
 
 conexao.on("error", (erro) => {
-    console.log("erro de conexão", erro);
+  console.error("erro de conexão", erro);
 });
 
 conexao.once("open", () => {
-    console.log("Conexão com o banco feita com sucesso!");
-});
+  console.log("Conexao com o banco feita com sucesso");
+})
 
 const app = express();
-app.use(express.json());
-
-app.get("/", (req, res) => {
-    res.status(200).send("Curso de Node.js");
-});
-
-app.get("/livros", async (req, res) => {
-    const listaLivros = await livro.find({ });
-    res.status(200).json(listaLivros);
-});
-
-app.get("/livros/:id", (req, res) => {
-    const index = buscarLivro(req.params.id);
-    res.status(200).json(livros[index]);
-})
-
-app.put("/livros/:id", (req, res) => {
-    const index = buscarLivro(req.params.id);
-    livros[index].titulo = req.body.titulo;
-    res.status(200).send("Atualizado com Sucesso!");
-})
-
-app.post("/livros", (req, res) => {
-    livros.push(req.body);
-    res.status(201).send("Cadastrado com sucesso!")
-});
+routes(app);
 
 app.delete("/livros/:id", (req, res) => {
-    const index = buscarLivro(req.params.id);
-    livros.splice(index, 1);
-    res.status(200).send("Excluído com Sucesso!");
-})
+  const index = buscaLivro(req.params.id);
+  livros.splice(index, 1);
+  res.status(200).send("livro removido com sucesso");
+});
 
 export default app;
